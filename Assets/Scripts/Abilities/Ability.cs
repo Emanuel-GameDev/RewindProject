e78199dc777c8d981c3ed81e00e99f5f4c35c26a
Emanuel-Creator <120394201+Emanuel-GameDev@users.noreply.github.com>
2023-04-27 17:53:02 +0200

    Started AbilityWheel

---
+++ Assets/Scripts/Abilities/Ability.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Ability : MonoBehaviour
+{
+    public new string name;
+    public string description;
+}
