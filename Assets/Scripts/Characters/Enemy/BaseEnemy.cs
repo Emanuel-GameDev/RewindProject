0eafdc6ccf908c1bb187e7b846ff945c7e646b5c
Kyldros <kyldros@gmail.com>
2023-04-30 13:14:51 +0200

    Update BaseEnemy.cs

--- Assets/Scripts/Characters/Enemy/BaseEnemy.cs
+++ Assets/Scripts/Characters/Enemy/BaseEnemy.cs
@@ -1,3 +1,5 @@
+using BehaviorDesigner.Runtime;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +7,57 @@ using UnityEngine.AI;
 
 public class BaseEnemy : MonoBehaviour
 {
-	void Start()
-	{
-		NavMeshAgent agent = GetComponent<NavMeshAgent>();
-		agent.updateRotation = false;
-		agent.updateUpAxis = false;
-	}
+    private SpriteRenderer spriteRenderer;
+    private NavMeshAgent navMeshAgent;
+    private BehaviorTree tree;
+    [SerializeField] float viewRotation = 90;
+    [SerializeField] string viewRotationVariableName = "View Rotation";
+
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        tree = GetComponentInChildren<BehaviorTree>();
+    }
+
+    private void Update()
+    {
+        FlipCharacter();
+        FlipView();
+    }
+
+    private void FlipView()
+    {
+        float value = viewRotation;
+        if(spriteRenderer.flipX)
+            value = -value;
+
+        tree.SetVariableValue(viewRotationVariableName, value);
+    }
+
+    private void FlipCharacter()
+    {
+        // Otteniamo la direzione del movimento
+        Vector3 moveDirection = navMeshAgent.desiredVelocity;
+
+        // Se la direzione non è nulla, effettuiamo il flip
+        if (moveDirection != Vector3.zero)
+        {
+            if (moveDirection.x < 0)
+            {
+                spriteRenderer.flipX = true;
+            }
+            else if (moveDirection.x > 0)
+            {
+                spriteRenderer.flipX = false;
+            }
+        }
+    }
 }
