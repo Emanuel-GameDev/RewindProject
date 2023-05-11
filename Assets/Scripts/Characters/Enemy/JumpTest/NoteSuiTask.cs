5c6d2912fc013513c317bc94bb1b82cd46ad36d1
Kyldros <kyldros@gmail.com>
2023-05-11 17:34:07 +0200

    Nuovi Task

--- Assets/Scripts/Characters/Enemy/JumpTest/NoteSuiTask.cs
+++ Assets/Scripts/Characters/Enemy/JumpTest/NoteSuiTask.cs
@@ -2,7 +2,8 @@
 // 1. Controllare se il target è più in alto o in basso
 //      Se è in Alto o in Basso:
 //          - Controllare se è nel range e se ci sono pedane vicine
-//          -
+//          - Avvicinarsi alla pedana
+//          - Saltare sulla Pedana
 //          -
 //      Se è alla stessa altezza:
 //          - Controllare se c'è terreno percorribile -> se Si lo segue
