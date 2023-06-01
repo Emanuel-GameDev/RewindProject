5887faec4f438d21c100654be6569ac4978f1630
Emanuel-Creator <120394201+Emanuel-GameDev@users.noreply.github.com>
2023-06-01 23:23:45 +0200

    Added small + medium platforms

---
+++ Assets/Scripts/Objects/Platforms/OneWayPlatform.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class OneWayPlatform : MonoBehaviour
+{
+    private BoxCollider2D playerCollider;
+    private bool coroutineRunning = false;
+
+    [SerializeField] private bool playerCanGoDown = true;
+
+    [Tooltip("Increase if platform too big or player will be snapped out, changes time for the ignore collision player-platform")]
+    [SerializeField] private float snapOffset = 1f;
+
+    private PlayerInputs inputs;
+
+
+    private void Start()
+    {
+        inputs = PlayerController.instance.inputs;
+
+        inputs.Player.Down.performed += StartPlatformDisabling;
+    }
+
+    private void OnDisable()
+    {
+
+        inputs.Player.Down.performed -= StartPlatformDisabling;
+    }
+
+    private void StartPlatformDisabling(InputAction.CallbackContext obj)
+    {
+        if (playerCanGoDown && !coroutineRunning && playerCollider != null && 
+            !playerCollider.gameObject.GetComponent<PlayerController>().isJumping)
+        {
+            StartCoroutine(DisablePlatform());
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Character>() != null
+            && !coroutineRunning)
+        {
+            Character character = collision.gameObject.GetComponent<Character>();
+            playerCollider = character.gameObject.GetComponent<BoxCollider2D>();
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Character>() != null
+            && !coroutineRunning)
+        {
+            playerCollider = null;
+        }
+    }
+
+    private IEnumerator DisablePlatform()
+    {
+        coroutineRunning = true;
+        PolygonCollider2D platCollider = GetComponent<PolygonCollider2D>();
+
+        // Disattivo collisione
+        if (playerCollider != null && platCollider != null)
+            Physics2D.IgnoreCollision(playerCollider, platCollider);
+
+        yield return new WaitForSeconds(snapOffset);
+
+        // Riattivo collisione
+        if (playerCollider != null && platCollider != null)
+            Physics2D.IgnoreCollision(playerCollider, platCollider, false);
+
+        coroutineRunning = false;
+
+    }
+}
