9e6090e0b9602998c6be3e8f4f03eb71288b952b
Kyldros <kyldros@gmail.com>
2023-07-09 16:25:46 +0200

    Revert "Merge branch 'Luca' into Leonardo"
    This reverts commit fa14fd57599b63bcd8e1e8f5e7c250c8c943965e, reversing
    changes made to 0f1278658087dc4b782b35d647e322f969b63d24.

--- Assets/Scripts/Objects/Platforms/MovingPlatform.cs
+++ Assets/Scripts/Objects/Platforms/MovingPlatform.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingPlatform : MonoBehaviour
+public class MoovingPlatform : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
