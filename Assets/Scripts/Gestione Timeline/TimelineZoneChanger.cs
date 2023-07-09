3788339a6d8eca26d5e44a8174e97d271b9545a2
AndreaSforzi <andreasforzi01@gmail.com>
2023-07-09 17:05:48 +0200

    sistemato errore

--- Assets/Scripts/Gestione Timeline/TimelineZoneChanger.cs
+++ Assets/Scripts/Gestione Timeline/TimelineZoneChanger.cs
@@ -55,7 +55,7 @@ public class TimelineZoneChanger : MonoBehaviour
     {
         if (startAtEnd)
         {
-            //TimelineManager.Instance.SetAtEnd(zone);
+            TimelineManager.Instance.SetAtEnd(zone);
         }
     }
 
