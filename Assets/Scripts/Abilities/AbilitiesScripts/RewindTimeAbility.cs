81221cb2520856959019253012d7aa0d68f65582
Emanuel-Creator <120394201+Emanuel-GameDev@users.noreply.github.com>
2023-08-03 19:16:14 +0200

    mgv

--- Assets/Scripts/Abilities/AbilitiesScripts/RewindTimeAbility.cs
+++ Assets/Scripts/Abilities/AbilitiesScripts/RewindTimeAbility.cs
@@ -6,30 +6,33 @@ public class RewindTimeAbility : Ability
     public override void Activate1(GameObject parent)
     {
         isActive = true;
-
+        Debug.Log("activate 1");
         TimelineManager.Instance.StartForwarding();
     }
 
     public override void Activate2(GameObject parent)
     {
         isActive = true;
-
+        Debug.Log("activate 2");
         TimelineManager.Instance.StartRewinding();
     }
 
     public override void Activate3(GameObject parent)
     {
+        Debug.Log("activate 3");
         TimelineManager.Instance.StartStopControlTimeline();
     }
 
     public override void Disactivate1(GameObject gameObject)
     {
+        Debug.Log("disactivate 1 ");
         TimelineManager.Instance.StopForwarding();
     }
 
     public override void Disactivate2(GameObject gameObject)
     {
         TimelineManager.Instance.StopRewinding();
+        Debug.Log("disactivate 2 ");
 
         isActive = false;
     }
