516949de5f6fd852fc9364372f8e50d1c9e3d23e
Emanuel-Creator <120394201+Emanuel-GameDev@users.noreply.github.com>
2023-08-17 12:44:58 +0200

    Added SUmmonTower ability placeholder

--- Assets/Scripts/Abilities/AbilitiesScripts/SummonTower.cs
+++ Assets/Scripts/Abilities/AbilitiesScripts/SummonTower.cs
@@ -1,20 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [CreateAssetMenu(menuName = "Ability/SummonTower")]
 public class SummonTower : Ability
 {
-    [SerializeField] GameObject towerPrefab;
+    [SerializeField] private GameObject towerPrefab;
+    [SerializeField] private Vector2 summonOffset;
+    [SerializeField] private float cooldown;
+
+    [Header("TOWER DATA")]
+    [SerializeField] private int hp;
+    [SerializeField] private LayerMask collisionMask;
+
+    private bool facingRight;
+    private PlayerInputs inputs;
+    private Tower currentTower;
+    private bool canActivate = true;
+    private float lastActivationTime;
+
+    private void OnEnable()
+    {
+        inputs = new PlayerInputs();
+
+        if (!inputs.Player.enabled)
+            inputs.Player.Enable();
+
+        inputs.Player.Walk.performed += CheckHorizontalFacing;
+        inputs.Player.Run.performed += CheckHorizontalFacing;
+
+    }
+
+    private void OnDisable()
+    {
+        inputs.Player.Walk.performed -= CheckHorizontalFacing;
+        inputs.Player.Run.performed -= CheckHorizontalFacing;
+    }
 
     public override void Activate1(GameObject parent)
     {
+        if (!canActivate || currentTower == null) return;
+
         PlayerController player = parent.GetComponent<PlayerController>();
 
-        // Evoco torre
-        if (player.grounded && player != null)
-            Instantiate(towerPrefab, player.gameObject.transform.position, player.gameObject.transform.rotation);
+        if (!player.grounded) return;
+
+        Vector2 summonPos;
+
+        if (facingRight)
+            summonPos = new Vector2(player.transform.position.x + summonOffset.x, player.transform.position.y + summonOffset.y);
+        else
+            summonPos = new Vector2(player.transform.position.x - summonOffset.x, player.transform.position.y + summonOffset.y);
+
+        currentTower.Activate(summonPos);
+
+        canActivate = false;
+        lastActivationTime = Time.time;
     }
 
-    
+    public override void UpdateAbility()
+    {
+        base.UpdateAbility();
+
+        if (!canActivate && Time.time >= lastActivationTime + cooldown)
+            canActivate = true;
+    }
+
+    public override void Pick(Character picker)
+    {
+        base.Pick(picker);
+
+        currentTower = Instantiate(towerPrefab).GetComponent<Tower>();
+        currentTower.Initialize(hp, collisionMask);
+    }
+
+    private void CheckHorizontalFacing(InputAction.CallbackContext obj)
+    {
+        float value = obj.ReadValue<float>();
+
+        if (value < 0)
+        {
+            facingRight = false;
+        }
+        else
+        {
+            facingRight = true;
+        }
+    }
+
 }
---
+++ Assets/Scripts/Abilities/AbilitiesScripts/Tower.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    private Animator animator;
+    private int maxHp;
+    private int hp;
+    private LayerMask collisionMask;
+
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Initialize(int hp, LayerMask mask)
+    {
+        maxHp = hp;
+        this.hp = maxHp;
+
+        collisionMask = mask;
+    }
+
+
+    public void Activate(Vector2 pos)
+    {
+        transform.position = pos;
+
+        animator.SetTrigger("Activated");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (((1 << collision.gameObject.layer) & collisionMask) != 0)
+        {
+            hp -= 1;
+
+            if (hp == 0)
+            {
+                animator.SetTrigger("Destroyed");
+                Dismiss();
+            }
+        }
+    }
+
+    public void Dismiss()
+    {
+        hp = maxHp;
+    }
+}
