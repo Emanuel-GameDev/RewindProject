bb4f9309524f358054b2b8c05b8a2f41c9bdb66e
AndreaSforzi <andreasforzi01@gmail.com>
2023-08-19 18:25:35 +0200

    Cooldown e audio provvisorio attacco

--- Assets/Scripts/Abilities/AbilitiesScripts/MeleeAttack.cs
+++ Assets/Scripts/Abilities/AbilitiesScripts/MeleeAttack.cs
@@ -1,13 +1,18 @@
+using System.Collections;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Ability/MeleeAttack")]
 public class MeleeAttack : Ability
 {
+  
     public override void Activate1(GameObject parent)
     {
         if (!PlayerController.instance.canAttack)
             return;
 
-            PlayerController.instance.Attack();
+        PlayerController.instance.cooldownMeleeAttack = cooldownTime;
+        PlayerController.instance.Attack();
     }
+
+   
 }
