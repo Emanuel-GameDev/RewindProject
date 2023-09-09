ff16635d9c8de1c94b7a439f4a2474e0ccdbac57
AndreaSforzi <andreasforzi01@gmail.com>
2023-09-09 15:43:26 +0200

    Animazione Idle player

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 
 public class MainMenu : Menu
 {
+    [SerializeField] MenuButton continueButton;
+
     public override void Start()
     {
         gameObject.SetActive(true);
+        if (DataSerializer.HasKey("HasSaving"))
+            continueButton.gameObject.SetActive(true);
+        else
+            continueButton.gameObject.SetActive(false);
+
+    }
+
+    public void GameStartedOnce()
+    {
+        DataSerializer.Save("HasSaving", true);
     }
 
     public void DeleteSaves()
