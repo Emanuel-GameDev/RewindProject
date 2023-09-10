21ea13f914ec6af0795e2bb93af94cc2fac3a411
AndreaSforzi <andreasforzi01@gmail.com>
2023-09-10 16:48:07 +0200

    audio

--- Assets/Scripts/AudioSystem/AudioManager.cs
+++ Assets/Scripts/AudioSystem/AudioManager.cs
@@ -23,6 +23,8 @@ public class AudioManager : MonoBehaviour
     {
         audioSource = GetComponent<AudioSource>();
         mixer = audioSource.outputAudioMixerGroup;
+
+        
     }
 
     void Start()
